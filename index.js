cee6894e634d3c02d7d9de04d11bffb44d62ce3e
Sarah Laloggia <54509406+slaloggia@users.noreply.github.com>
2019-11-24 17:56:16 -0600

    Done.

--- index.js
+++ index.js
@@ -1,2 +1,6 @@
-
-// Write your code here
+let num1 = 2
+let num2 = 31
+const multiply = num1 * num2
+const random = Math.ceil(Math.random()*10)
+const mod = 24 % 5
+const max = Math.max(2, 5, 20, 18, 9)
